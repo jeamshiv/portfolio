64817fb0704fb631b1bb719ce5b0350163196857
Shiv <jeamshiv@gmail.com>
2021-11-07 15:47:31 +0530

    updated memory data

---
+++ src/MemoryData/ProjectsData.js
@@ -0,0 +1,204 @@
+
+
+    export const FeaturedWordData = 
+        {
+            // description: "As a web and mobile app developer, I have had many amazing associations that have led to some remarkable products. Here I have shared some of my recent and most notable work in web and mobile app development.",
+            description: "This is movie search app create using React Native. This app will help you to find out about the movie, poster, casts, trailer and casts details and many more.",
+            bannerImg : "https://raw.githubusercontent.com/jeamshiv/portfolio/main/src/MemoryData/image/app/MovieAppShiv.webp",
+            projectLink : "/app/5"
+        }
+    
+
+
+    export const AllProjectsData = [
+       
+
+       {
+        projectId: 5,
+        title: "MovieApp Shiv",
+        description: "This is movie search app create using React Native. This app will help you to find out about the movie, poster, casts, trailer and casts details and many more.",
+        type : "App",
+        link : "https://www.google.com",
+        headerImage : "https://raw.githubusercontent.com/jeamshiv/me/main/src/MemoryData/image/app/MovieAppShiv.webp",
+        technologies : "React",
+        features : [
+            {title: "User Friendly", 
+             description: "Most provably best dashboard design for your business you can try."
+           },
+            {title: "Award Winning App",
+             description: "Most provably best dashboard design for your business you can try."
+           },
+           {title: "Privacy Protected",
+            description: "Most provably best dashboard design for your business you can try."
+           },
+           {title: "Lifetime Update",
+            description: "Most provably best dashboard design for your business you can try."}
+        ],
+        mainContent : [
+            {heading: "Why choose us",
+             body: "The quick brown fox jumps over the lazy dog"
+             },
+             {heading: "Second Heading",
+             body: "The quick brown fox jumps 2 wala"}
+        ],
+        fullScreenShot : null,
+        screenshots : [
+            {img: "",
+             des:"Home page hai yeh"},
+             {img: "",
+             des:"Yeh contact Page hai"}
+        ]
+    },
+
+    {
+        projectId: 4,
+        title: "Movie Search",
+        description: "The Movie Search website help to get the all information about the searched movie. You can get the brief information about the movie using the movie search web app.",
+        type : "Website",
+        link : "https://jeamshiv.github.io/movie/",
+        headerImage : "https://raw.githubusercontent.com/jeamshiv/me/main/src/MemoryData/image/web/first-movie.webp",
+        technologies : "Html, Css, JavaScript, Bootstrap-3,",
+        features : [
+        {title: "Mobile First", 
+        description: "Most provably best dashboard design for your business you can try."
+    },
+        {title: "Optimized Code",
+        description: "Most provably best dashboard design for your business you can try."
+    },
+    {title: "Free SEO",
+        description: "Most provably best dashboard design for your business you can try."
+    },
+    {title: "6 Months Support",
+        description: "Most provably best dashboard design for your business you can try."}
+        ],
+        mainContent : [
+            {heading: "Why Jeamshiv blogger",
+            body: "The gradient color and the theme shape make jeamshiv blogspot website unique from anything else and it have simple navigation."
+            },
+            {heading: "Other Import Secret",
+            body: "It has working contact form which mean if you contact throught this website I and you both will get a mail from it and there is also live chat option there."}
+        ],
+        fullScreenShot : null,
+        screenshots : [
+            {img: "",
+            des:"Home page hai yeh"},
+            {img: "",
+            des:"Yeh contact Page hai"}
+        ]
+    },
+
+    {
+        projectId: 3,
+        title: "Jeamshiv Blogspot",
+        description: "This is my blogpost site which is highly customized and I tried to make to creative and awesome UI features.",
+        type : "Website",
+        link : "https://jeamshiv.blogspot.com",
+        headerImage : "https://raw.githubusercontent.com/jeamshiv/me/main/src/MemoryData/image/web/jeamshivBlogspot.webp",
+        technologies : "Html, Css, JavaScript, Bootstrap-4, Wow, Slick",
+        features : [
+        {title: "Mobile First", 
+        description: "Most provably best dashboard design for your business you can try."
+    },
+        {title: "Optimized Code",
+        description: "Most provably best dashboard design for your business you can try."
+    },
+    {title: "Free SEO",
+        description: "Most provably best dashboard design for your business you can try."
+    },
+    {title: "6 Months Support",
+        description: "Most provably best dashboard design for your business you can try."}
+        ],
+        mainContent : [
+            {heading: "Why Jeamshiv blogger",
+            body: "The gradient color and the theme shape make jeamshiv blogspot website unique from anything else and it have simple navigation."
+            },
+            {heading: "Other Import Secret",
+            body: "It has working contact form which mean if you contact throught this website I and you both will get a mail from it and there is also live chat option there."}
+        ],
+        fullScreenShot : null,
+        screenshots : [
+            {img: "",
+            des:"Home page hai yeh"},
+            {img: "",
+            des:"Yeh contact Page hai"}
+        ]
+    },
+
+    {
+        projectId: 2,
+        title: "Best Restaurant",
+        description: "This is html template for Resturant purposes create using HTML, Css and JavaScript and Bootstrap-4, Wow and Slick to make to awesome and beautiful theme. It's one page website which can eaily use for Restaurant or Pizza Shop.",
+        type : "Website",
+        link : "https://jeamshiv.github.io/restaurant/",
+        headerImage : "https://raw.githubusercontent.com/jeamshiv/me/main/src/MemoryData/image/web/Resturant.webp",
+        technologies : "Html, Css, JavaScript, Bootstrap-4, Wow, Slick",
+        features : [
+        {title: "Mobile First", 
+        description: "Most provably best dashboard design for your business you can try."
+    },
+        {title: "Optimized Code",
+        description: "Most provably best dashboard design for your business you can try."
+    },
+    {title: "Free SEO",
+        description: "Most provably best dashboard design for your business you can try."
+    },
+    {title: "6 Months Support",
+        description: "Most provably best dashboard design for your business you can try."}
+        ],
+        mainContent : [
+            {heading: "Why Study Hub",
+            body: "The gradient color and the theme shape make Study Hub theme unique from anything else and it have simple navigation."
+            },
+            {heading: "Other Import Secret",
+            body: "It has working contact form which mean if you contact throught this website I and you both will get a mail from it and there is also live chat option there."}
+        ],
+        fullScreenShot : null,
+        screenshots : [
+            {img: "",
+            des:"Home page hai yeh"},
+            {img: "",
+            des:"Yeh contact Page hai"}
+        ]
+    },
+
+
+
+        {
+            projectId: 1,
+            title: "Study Hub",
+            description: "This is html template for educational purposes create using HTML, Css and JavaScript and Bootstrap. It's one page website which can eaily use for educational purpose.",
+            type : "Website",
+            link : "https://jeamshiv.github.io/studyhub.io/",
+            headerImage : "https://raw.githubusercontent.com/jeamshiv/me/0312804865dd9aec6b0355939f00dcf210594732/src/MemoryData/image/web/StudyHub.webp",
+            technologies : "Html, Css, JavaScript",
+            features : [
+            {title: "Mobile First", 
+            description: "Most provably best dashboard design for your business you can try."
+        },
+            {title: "Optimized Code",
+            description: "Most provably best dashboard design for your business you can try."
+        },
+        {title: "Free SEO",
+            description: "Most provably best dashboard design for your business you can try."
+        },
+        {title: "6 Months Support",
+            description: "Most provably best dashboard design for your business you can try."}
+            ],
+            mainContent : [
+                {heading: "Why Study Hub",
+                body: "The gradient color and the theme shape make Study Hub theme unique from anything else and it have simple navigation."
+                },
+                {heading: "Other Import Secret",
+                body: "It has working contact form which mean if you contact throught this website I and you both will get a mail from it and there is also live chat option there."}
+            ],
+            fullScreenShot : null,
+            screenshots : [
+                {img: "",
+                des:"Home page hai yeh"},
+                {img: "",
+                des:"Yeh contact Page hai"}
+            ]
+        },
+
+    ]
+    
