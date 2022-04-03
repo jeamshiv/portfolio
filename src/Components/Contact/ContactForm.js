e234569828ba4a2fce958ffe66efe4f3ac024f2b
Shiv <jeamshiv@gmail.com>
2022-04-03 16:52:34 +0530

    updated netlify form

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import TextField from '@mui/material/TextField';
-import ClipLoader from "react-spinners/ClipLoader";
-import {contactRequest} from '../../ApiConnection/Contact'
+import SyncLoader from "react-spinners/SyncLoader";
 import WOW from 'wowjs'
 
-export default class ContactForm extends Component {
+export default function ContactForm() {
 
-    state = {
-        isLoading: false,
-    }
+    const[isLoading, setIsLoading] = useState(false)
 
-    componentDidMount() {
+    useEffect(() => {
         new WOW.WOW({
-            live:false
+            live: false
         }).init();
-    }
+    }, [])
+    
 
 
-    renderContactCard=(icon, title, des)=>{
-        return(
+    const renderContactCard = (icon, title, des) => {
+        return (
             <div className="col-12 mt-3 wow">
                 <div className="d-flex py-3">
                     <div className="mx-3 align-self-center">
@@ -33,43 +31,9 @@ export default class ContactForm extends Component {
         )
     }
 
-
-    changeHandler=(e)=>{
-        console.log(e.target.name, e.target.value)
-        console.log('getting contact at contactfomr')
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-
-    }
-
-
-    clickHandler=()=>{
-        console.log('show all on click', this.state)
-        this.setState({isLoading: true})
-        alert('Sorry!, contact me directly.')
-        this.setState({isLoading: false})
-        contactRequest(this.state.name, this.state.email, this.state.phone, this.state.message, this.contactCallback)
-    }
-
-    contactCallback=(response)=>{
-        if(response.status == 200){
-            response.json().then(data =>
-                {   
-                   console.log('check status now')
-                   
-                })
-        }
-        this.setState({isLoading: false})
-    }
-
-
-    
-
-    render() {
-        return (
-            <>
-            <section className="contact container pt-5 mt-5">
+    return (
+        <>
+            <section className="contact container py-5 my-5">
                 <div className="text-center mb-3">
                     <h5>Get in Touch</h5>
                     <h1 className="style-title">Contact Me</h1>
@@ -79,28 +43,35 @@ export default class ContactForm extends Component {
                     <div className="col-lg-8 my-3 mb-lg-5 wow bounceInUp">
 
                         <div className="wrapper p-4">
-                            
+
                             <p><b>SAY SOMETHING</b></p>
-                           
-                            <TextField className="form-control mt-3" name="name" label="Your Name" onChange={(e)=>this.changeHandler(e)} />
 
-                            <TextField className="form-control mt-3" name="email" label="Your Email" onChange={(e)=>this.changeHandler(e)} />
+                            <form method="POST" name='PortfolioContactForm' className='PortfolioContactForm' action='/confirmation-page'>
 
-                            <TextField className="form-control mt-3" name="phone" label="Your Phone" onChange={(e)=>this.changeHandler(e)} inputProps={{ maxLength: 10 }} />
-                            
-                            <TextField
-                                className="form-control mt-3"
-                                label="Message"
-                                name="message"
-                                multiline
-                                rows={4}
-                                onChange={(e)=>this.changeHandler(e)}
+                                <input type="hidden" name="form-name" value="PortfolioContactForm" />
+
+                                <TextField className="form-control mt-3" name="name" label="Your Name" />
+
+                                <TextField className="form-control mt-3" name="email" label="Your Email" />
+
+                                <TextField className="form-control mt-3" name="phone" label="Your Phone" inputProps={{ maxLength: 10 }} />
+
+                                <TextField
+                                    className="form-control mt-3"
+                                    label="Message"
+                                    name="message"
+                                    multiline
+                                    rows={4}
                                 />
 
 
-                            <div className="mt-5">
-                                {this.state.isLoading?(<button className="primary-button" >&nbsp;<ClipLoader color="white" loading="MoonLoader" style={{paddingLeft: 40, paddingRight: 40}} size={18} /></button>):(<button className="primary-button" onClick={() => this.clickHandler()}>Send Message</button>)}
-                            </div>
+                                <div className="mt-5">
+                                    {isLoading ? 
+                                    (<button className="primary-button" >&nbsp;<SyncLoader color="white" size={12} margin={3} /></button>) : 
+                                    (<button className="primary-button" onClick={() => setIsLoading(true)}>Send Message</button>)}
+                                </div>
+
+                            </form>
 
                         </div>
 
@@ -108,16 +79,15 @@ export default class ContactForm extends Component {
                     <div className="col-lg-4">
                         <div className="row px-4">
 
-                            {this.renderContactCard('fas fa-map-marker-alt', 'My Address', 'Faridabad Haryana')}
-                            {this.renderContactCard('fas fa-phone-alt', 'My Phone', '+91 8750303073')}
-                            {this.renderContactCard('far fa-envelope-open', 'My Email', 'jeamshiv@gmail.com')}
-                            
+                            {renderContactCard('fas fa-map-marker-alt', 'My Address', 'Faridabad Haryana')}
+                            {renderContactCard('fas fa-phone-alt', 'My Phone', '+91 8750303073')}
+                            {renderContactCard('far fa-envelope-open', 'My Email', 'jeamshiv@gmail.com')}
+
                         </div>
                     </div>
                 </div>
-                
-            </section>  
-            </>
-        )
-    }
+
+            </section>
+        </>
+    )
 }
